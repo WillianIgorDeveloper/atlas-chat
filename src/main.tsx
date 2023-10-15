0ea59f6181ebbcd2fe8b738242a02f54ab8f5886
Willian Igor <willianigordeveloper@gmail.com>
2023-10-15 15:21:05 -0300

    feature: finishing Home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "@/pages/home/Home";
+import { ThemeProvider } from "./contexts/Theme";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<ThemeProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<Home />} />
+				</Routes>
+			</BrowserRouter>
+		</ThemeProvider>
+	</React.StrictMode>
+);
