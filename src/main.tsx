4b356152bfa12cc89585c411a024946202bf5e46
Willian Igor Santos <willianigordeveloper@gmail.com>
2023-10-15 11:39:57 -0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
