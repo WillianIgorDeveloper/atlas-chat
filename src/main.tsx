824aac049e3dcacb54734380bdf2e80929c4fe2f
Willian Igor <willanigordeveloper@gmail.com>
2024-08-20 09:00:53 -0300

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "@/pages/home/Home";
-import { ThemeProvider } from "./contexts/Theme";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<ThemeProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route index element={<Home />} />
-				</Routes>
-			</BrowserRouter>
-		</ThemeProvider>
-	</React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<StrictMode />)
