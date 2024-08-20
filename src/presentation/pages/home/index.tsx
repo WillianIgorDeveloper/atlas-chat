682d20b6a4b69da4fa65a30b51ae3765a45a365c
Willian Igor <willanigordeveloper@gmail.com>
2024-08-20 10:00:23 -0300

    Feat: home page

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -1,3 +1,38 @@
+import { Button } from "@ui/button"
+import { ROUTES } from "@/utils/routes"
+import { ChevronsRightIcon } from "lucide-react"
+import { Link } from "react-router-dom"
+
 export function HomePage() {
-  return <></>
+  return (
+    <>
+      {/* <header className="fixed w-full">
+        <div className="container mx-auto p-3 flex items-center justify-between lg:p-5">
+          <h1 className="text-xl font-bold">Atlas chat</h1>
+          <Link to={ROUTES.LOGIN}>
+            <Button>Get Started</Button>
+          </Link>
+        </div>
+      </header> */}
+
+      <main>
+        <section className="text-center p-5 h-screen flex flex-col items-center justify-center gap-3 container max-w-sm md:flex-row-reverse md:text-left md:max-w-none md:gap-5 md:h-[80vh] lg:gap-8 xl:gap-28">
+          <img
+            src="/icon.png"
+            alt="Atlas"
+            className="animate-fade-down duration-500 md:max-w-xs"
+          />
+          <div className="space-y-3 animate-fade-up duration-500 md:max-w-xs lg:max-w-md xl:max-w-lg">
+            <h2 className="text-3xl font-black bg-primary text-transparent bg-clip-text lg:text-4xl xl:text-5xl">
+              Connect with friends, colleagues, and loved ones effortlessly.
+            </h2>
+            <p className="px-3 md:p-0">
+              Enjoy real-time messaging, and group chats. Stay connected, anytime,
+              anywhere.
+            </p>
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -60,10 +60,40 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "fade-in": {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
+        "fade-out": {
+          from: { opacity: "1" },
+          to: { opacity: "0" },
+        },
+        "fade-up": {
+          from: { opacity: "0", transform: "translateY(8px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        "fade-down": {
+          from: { opacity: "0", transform: "translateY(-8px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        "fade-left": {
+          from: { opacity: "0", transform: "translateX(8px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
+        "fade-right": {
+          from: { opacity: "0", transform: "translateX(-8px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-in": "fade-in 0.2s ease-out",
+        "fade-out": "fade-out 0.2s ease-out",
+        "fade-up": "fade-up 0.2s ease-out",
+        "fade-down": "fade-down 0.2s ease-out",
+        "fade-left": "fade-left 0.2s ease-out",
+        "fade-right": "fade-right 0.2s ease-out",
       },
     },
   },
